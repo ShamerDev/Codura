f90cb4ee291151f5ea2b0f8f9d03f9b39801a093
Shamer <shamershahril.official@gmail.com>
2025-10-06 22:09:48 +0800

    Fix typo in project description header

--- resources/views/livewire/user/addentry.blade.php
+++ resources/views/livewire/user/addentry.blade.php
@@ -326,7 +326,7 @@ new class extends Component {
                                 d="M9 12h6m-6-4h6m2 5.291A7.962 7.962 0 0112 15c-2.34 0-4.29-1.009-5.824-2.562M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9">
                             </path>
                         </svg>
-                        Project Description
+                        Project Descriptions
                     </h2>
                 </div>
                 <div class="p-8 space-y-6">
